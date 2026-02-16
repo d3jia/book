1ed1941378ef58052d1e2f5e850b0109b9cc0dd3
DeJia <45155312+d3jia@users.noreply.github.com>
2026-02-16 15:40:52 +0800

    Move alias check to the end of zshrc_bare_env.sh
    Repositioned the check for 'alias ll=' to the end of the file.

--- zshrc_bare_env.sh
+++ zshrc_bare_env.sh
@@ -121,11 +121,6 @@ function cd {
 }
 EOF
 
-# Check if "alias ll=" exists; if not, add Ubuntu defaults
-if ! grep -q "^alias ll=" ~/.bashrc; then
-    echo -e "\n# Ubuntu Default ls Aliases\nalias l='ls -CF'\nalias ll='ls -alF'\nalias la='ls -A'" >> ~/.bashrc
-fi
-
 # =========================
 # Vim config → ~/.vimrc
 # =========================
@@ -156,3 +151,8 @@ set shellcmdflag=-ic
 filetype plugin indent on
 syntax on
 EOF
+
+# Check if "alias ll=" exists; if not, add Ubuntu defaults
+if ! grep -q "^alias ll=" ~/.bashrc; then
+    echo -e "\n# Ubuntu Default ls Aliases\nalias l='ls -CF'\nalias ll='ls -alF'\nalias la='ls -A'" >> ~/.bashrc
+fi
